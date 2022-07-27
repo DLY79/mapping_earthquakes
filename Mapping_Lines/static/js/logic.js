b9244722417dc96c5187588cd109789e6068e079
DLY79 <lyndsdavid@gmail.com>
2022-07-27 11:32:49 -0300

    We made lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,7 +2,20 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -96.2437], 4);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+  // Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
@@ -24,7 +37,7 @@ cityData.forEach(function(city) {
   .addTo(map);
 });
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
