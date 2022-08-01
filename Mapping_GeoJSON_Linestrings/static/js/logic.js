b10e88e7809f5c8351ccc9a6bbb65bcca364504a
DLY79 <lyndsdavid@gmail.com>
2022-08-01 15:54:32 -0300

    Adding Toronto data

---
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -0,0 +1,47 @@
+// Create the map object with center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
+
+// We create the tile layer that will be the background of our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Light: light,
+    Dark: dark
+  };
+
+  // Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [44.0, -80.0],
+    zoom: 2,
+    layers: [light]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/DLY79/mapping_earthquakes/main/Mapping_Multiple_GeoJSON/majorAirports.json";
+// Then we add our 'graymap' tile layer to the map.
+
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+
+
+
+
+
