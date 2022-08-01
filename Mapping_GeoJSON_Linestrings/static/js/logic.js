76c392f0f92631189f685fc7af5eb05b42674a2a
DLY79 <lyndsdavid@gmail.com>
2022-08-01 16:02:25 -0300

    Added airport linestrings and changed formatting

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -30,15 +30,20 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/DLY79/mapping_earthquakes/main/Mapping_Multiple_GeoJSON/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/DLY79/mapping_earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 // Then we add our 'graymap' tile layer to the map.
 
-
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+  color: "#ffffa1",
+  weight: 2,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3>  <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+  }
+}).addTo(map);
 });
 
 
